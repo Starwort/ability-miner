c43a66205cf4df66300176d304000c96deb65875
starwort <tcphone93@gmail.com>
2022-10-01 08:35:10 +0100

    Add main file to test if algorithm works

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+use std::env::args;
+use std::string::ParseError;
+
 use ability_miner::*;
 
 fn main() {
-    println!("Hello, world!");
+    let mut seed = args()
+        .next()
+        .expect("Missing seed")
+        .parse::<u32>()
+        .expect("Not a valid seed");
+    let brand = args()
+        .next()
+        .expect("Missing brand")
+        .parse::<Brand>()
+        .expect("Not a valid brand");
+    let drink = args()
+        .next()
+        .map(|ability| ability.parse().expect("not a valid ability"));
+    for _ in 0..10 {
+        println!("{seed}, {}", get_ability(&mut seed, brand, drink))
+    }
+    println!("{seed}");
 }
